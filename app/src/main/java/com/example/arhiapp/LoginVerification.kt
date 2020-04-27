432137d91169d26412716da9ccd9ba3300ff14b1
Moisei Mihai <mihai.moisei@gmail.com>
2020-04-27 15:00:15 +0300

    Si a doua parola

--- app/src/main/java/com/example/arhiapp/LoginVerification.kt
+++ app/src/main/java/com/example/arhiapp/LoginVerification.kt
@@ -2,7 +2,9 @@ package com.example.arhiapp
 
 class LoginVerification {
     fun verify(email: String, password: String): Boolean {
-        return email == "misu@arhimedes.ro" &&
-                password == "mogosa.2010"
+        return (email == "misu@arhimedes.ro" &&
+                password == "mogosa.2010") ||
+                (email == "mihai.moisei@gmail.com" &&
+                password == "mogosa.19")
     }
 }
