e9718e1f0f5d0da6e8e4e6ce4925b6a24df17bbe
Moisei Mihai <mihai.moisei@gmail.com>
2020-04-27 15:39:07 +0300

    Alte modificari

--- app/src/main/java/com/example/arhiapp/LoginVerification.kt
+++ app/src/main/java/com/example/arhiapp/LoginVerification.kt
@@ -3,8 +3,8 @@ package com.example.arhiapp
 class LoginVerification {
     fun verifyCredentials(email: String, password: String): Boolean {
         return (email == "misu@arhimedes.ro" &&
-                password == "mogosa.2010") ||
+                 password == "mogosa.2000") ||
                 (email == "mihai.moisei@gmail.com" &&
-                password == "mogosa.19")
+                password == "ignis.19")
     }
 }
